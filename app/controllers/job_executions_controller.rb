1e69b508b3f58af8eabaed8ee4adf0adc04f047d
Ivan Filin <filin87@gmail.com>
2021-12-16 16:57:17 +0300

    First step

--- app/controllers/job_executions_controller.rb
+++ app/controllers/job_executions_controller.rb
@@ -1,5 +1,27 @@
 class JobExecutionsController < ApplicationController
+  before_action :find_job_executions, only: %i[index]
+  
   def index
 
+  end
+
+  def new
+    @job_execution = JobExecution.new
+    byebug
+  end
+
+  def create
+    # @test = current_user.created_tests.new(test_params)
+    # if @test.save
+    #   redirect_to admin_tests_path, notice: t('.success')
+    # else 
+    #   render :new
+    # end
+  end
+
+  private
+
+  def find_job_executions
+    @job_executions = JobExecution.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "jobs#index"
+  root to: "job_executions#index"
   resources :job_executions
   resources :jobs
 end
